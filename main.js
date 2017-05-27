885b7a8002642c16c943559dc443c6c596159440
Sean Laing <seanlain@seanlain-macbook.local>
2017-05-26 21:16:58 -0700

    added timed writes

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const noble = require('noble');
-const stats = require("stats-lite");
+const fs = require('fs');
+const commandLineArgs = require('command-line-args')
+const path = require('path')
+
+const optionDefinitions = [
+  { name: 'output', type: String },
+]
+const options = commandLineArgs(optionDefinitions)
+
+let stringBuilder = "";
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
@@ -13,7 +22,7 @@ noble.on('error', function(error){
   console.log("ERROR: " + error);
 });
 
-const devices = {};
+let devices = {};
 
 
 noble.on('discover', function(peripheral) {
@@ -23,9 +32,17 @@ noble.on('discover', function(peripheral) {
     devices[peripheral.address] = device;
   }
   device.rssi.push(peripheral.rssi);
-  console.log(Math.round(Date.now()/1000) + "\t" + peripheral.address + "\t" + peripheral.rssi);
+  stringBuilder += (Math.round(Date.now()/1000) + "\t" + peripheral.address + "\t" + peripheral.rssi);
 });
 
 setTimeout(function() {
   process.exit(1);
-},1000*60*60); //gather data for one hour
+}, 1000*60*60); //kill after one hour, no matter what
+
+setTimeout(() => {
+  const filepath = path.join(options.output,Math.round(Date.now()/1000) + '.tsv');
+  fs.writeFile(filepath, stringBuilder, 'utf8', (error) => {
+    if(error) console.log('error while trying to write file : ' + error);
+  });
+  stringBuilder = "";
+}, 1000*60); //log and clear buffer every minute  
